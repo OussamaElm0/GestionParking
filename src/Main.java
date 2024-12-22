7bf990d4419ecde4a1186867f9885629d5296777
OussamaElm0 <oussama.elmabrouki26@gmail.com>
2024-12-22 19:28:54 +0100

    Create a procedure to display menu options

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello y'all!");
+        System.out.println("Welcome to our parking management application!");
+        displayMenuOptions();
+    }
+
+    private static void displayMenuOptions(){
+        System.out.println("""
+                -----------------------------------------------------------
+                Choose by number :
+                1. Park a vehicule
+                2. Remove a car from parking
+                3. Display parking
+                -----------------------------------------------------------
+                """);
     }
 }
