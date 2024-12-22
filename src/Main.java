6af505a7c22883c11069849c3192bf260a2bd9b7
OussamaElm0 <oussama.elmabrouki26@gmail.com>
2024-12-22 19:42:18 +0100

    Add main loop

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,41 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int choice;
+
         System.out.println("Welcome to our parking management application!");
-        displayMenuOptions();
+        MAIN_LOOP : while(true){ // giving the loop a name to avoid break issues within the switch statement
+            displayMenuOptions();
+
+            choice = sc.nextInt();
+
+            switch (choice){
+                case 0:
+                    System.out.println("See you next time \uD83D\uDE01. Goodbye!");
+                    break MAIN_LOOP; // break the while loop
+                case 1 :
+                    System.out.println("You've choose to park a vehicule.");
+                    break;
+                case 2:
+                    System.out.println("You've choose to remove a vehicule.");
+                    break;
+                case 3:
+                    System.out.println("You've choose to display parked vehicules.");
+                    break;
+                default:
+                    System.out.println("Sorry! this option isn't available.");
+                    break;
+            }
+        }
     }
 
     private static void displayMenuOptions(){
         System.out.println("""
                 -----------------------------------------------------------
                 Choose by number :
+                0. To quit
                 1. Park a vehicule
                 2. Remove a car from parking
                 3. Display parking
