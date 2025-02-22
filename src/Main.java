ae7c1d35fbeba50605c176810e8140d48820c6a7
OussamaElm0 <oussama.elmabrouki26@gmail.com>
2025-02-22 10:58:22 +0100

    Refactor: Refactor Main class to improve readability and apply SRP

--- src/Main.java
+++ src/Main.java
@@ -17,8 +17,6 @@ public class Main {
     public static ResourceBundle bundle = ResourceBundle.getBundle("ressources.messages", currentLocale);
 
     public static void main(String[] args){
-
-
         Scanner sc = new Scanner(System.in);
         int choice;
 
@@ -34,94 +32,16 @@ public class Main {
                         System.out.println(bundle.getString("goodbye_message"));
                         break MAIN_LOOP; // break the while loop
                     case 1 :
-                        System.out.println(bundle.getString("park_vehicle"));
-                        Vehicule vehicule = null;
-                        System.out.println(bundle.getString("choose_type"));
-                        int vehiculeType = sc.nextInt();
-                        String[] vehiculeInformations;
-
-                        switch (vehiculeType){
-                            case 1:
-                                System.out.println(bundle.getString("enter_information_format_car"));
-                                 vehiculeInformations = sc.next()
-                                        .split("_");
-
-                                try {
-                                    if (vehiculeInformations.length == 2) {
-                                        vehicule = VehiculeFactory.createVehicule(VehiculeTypes.VOITURE, vehiculeInformations[0], Integer.parseInt(vehiculeInformations[1]));
-                                    } else if (vehiculeInformations.length == 1) {
-                                        vehicule = VehiculeFactory.createVehicule(VehiculeTypes.VOITURE, vehiculeInformations[0]);
-                                    }
-                                    vehicule.park();
-                                } catch (NumberFormatException e) {
-                                    System.out.println(bundle.getString("number_format_error"));
-                                }
-                                break;
-                            case 2:
-                                System.out.println(bundle.getString("enter_information_format_moto"));
-                                vehiculeInformations = sc.next()
-                                        .split("_");
-
-                                if(vehiculeInformations.length == 1){
-                                    vehicule = VehiculeFactory.createVehicule(VehiculeTypes.MOTO,vehiculeInformations[0]);
-                                } else if (vehiculeInformations.length == 2) {
-                                    vehicule = VehiculeFactory.createVehicule(VehiculeTypes.MOTO, vehiculeInformations[0], vehiculeInformations[1]);
-                                }
-                                vehicule.park();
-                                break;
-                            case 3:
-                                System.out.println(bundle.getString("enter_information_format_truck"));
-                                vehiculeInformations = sc.next()
-                                        .split("_");
-
-                                try{
-                                    if(vehiculeInformations.length == 1){
-                                        vehicule = VehiculeFactory.createVehicule(VehiculeTypes.CAMION, vehiculeInformations[0]);
-                                    } else if(vehiculeInformations.length == 2){
-                                        vehicule = VehiculeFactory.createVehicule(VehiculeTypes.CAMION, vehiculeInformations[0], vehiculeInformations[1]);
-                                    }
-                                    vehicule.park();
-                                } catch (ClassCastException e) {
-                                    System.out.println(bundle.getString("number_format_error"));
-                                }
-
-                                break;
-                            default:
-                                System.out.println(bundle.getString("option_not_available"));
-                        }
-
+                        parkVehicleOption(sc);
                         break;
                     case 2:
-                        System.out.println(bundle.getString("remove_vehicle"));
-                        System.out.println(bundle.getString("enter_matricule"));
-                        try {
-                            String matricule = sc.next();
-                            int vehiculeToFind = Parking.retirerVehicule(matricule);
-                            if(vehiculeToFind < 0){
-                                throw new VehiculeNotFoundException(bundle.getString("vehicle_not_found"));
-                            }
-
-                            System.out.println(bundle.getString("vehicle_removed"));
-                            System.out.println(
-                                    bundle.getString("free_places")
-                                            .replace("{0}", Integer.toString(Parking.placesDisponibles()))
-                            );
-                        } catch (VehiculeNotFoundException e) {
-                            System.out.println(e);
-                        }
-
+                        removeVehicleOption(sc);
                         break;
                     case 3:
-                        System.out.println(bundle.getString("display_parking"));
-                        try {
-                            Parking.displayParking();
-                        } catch (ParkingEmptyException e){
-                            System.out.println(e.getMessage());
-                        }
+                        displayParkingOption();
                         break;
                     case 4:
-                        System.out.println(bundle.getString("display_amount"));
-                        System.out.println(Parking.getTotalAmount());
+                        displayTotalAmountOption();
                         break;
                     default:
                         System.out.println(bundle.getString("option_not_available"));
@@ -140,5 +60,97 @@ public class Main {
                 -----------------------------------------------------------
                 """ + bundle.getString("choose_menu") + """
                 \n-----------------------------------------------------------""");
+    }
+
+    private static void parkVehicleOption(Scanner sc){
+        System.out.println(bundle.getString("park_vehicle"));
+        Vehicule vehicule = null;
+        System.out.println(bundle.getString("choose_type"));
+        int vehiculeType = sc.nextInt();
+        String[] vehiculeInformations;
+
+        switch (vehiculeType){
+            case 1:
+                System.out.println(bundle.getString("enter_information_format_car"));
+                vehiculeInformations = sc.next()
+                        .split("_");
+
+                try {
+                    if (vehiculeInformations.length == 2) {
+                        vehicule = VehiculeFactory.createVehicule(VehiculeTypes.VOITURE, vehiculeInformations[0], Integer.parseInt(vehiculeInformations[1]));
+                    } else if (vehiculeInformations.length == 1) {
+                        vehicule = VehiculeFactory.createVehicule(VehiculeTypes.VOITURE, vehiculeInformations[0]);
+                    }
+                    vehicule.park();
+                } catch (NumberFormatException e) {
+                    System.out.println(bundle.getString("number_format_error"));
+                }
+                break;
+            case 2:
+                System.out.println(bundle.getString("enter_information_format_moto"));
+                vehiculeInformations = sc.next()
+                        .split("_");
+
+                if(vehiculeInformations.length == 1){
+                    vehicule = VehiculeFactory.createVehicule(VehiculeTypes.MOTO,vehiculeInformations[0]);
+                } else if (vehiculeInformations.length == 2) {
+                    vehicule = VehiculeFactory.createVehicule(VehiculeTypes.MOTO, vehiculeInformations[0], vehiculeInformations[1]);
+                }
+                vehicule.park();
+                break;
+            case 3:
+                System.out.println(bundle.getString("enter_information_format_truck"));
+                vehiculeInformations = sc.next()
+                        .split("_");
+
+                try{
+                    if(vehiculeInformations.length == 1){
+                        vehicule = VehiculeFactory.createVehicule(VehiculeTypes.CAMION, vehiculeInformations[0]);
+                    } else if(vehiculeInformations.length == 2){
+                        vehicule = VehiculeFactory.createVehicule(VehiculeTypes.CAMION, vehiculeInformations[0], vehiculeInformations[1]);
+                    }
+                    vehicule.park();
+                } catch (ClassCastException e) {
+                    System.out.println(bundle.getString("number_format_error"));
+                }
+
+                break;
+            default:
+                System.out.println(bundle.getString("option_not_available"));
+        }
+    }
+
+    private static void removeVehicleOption(Scanner sc){
+        System.out.println(bundle.getString("remove_vehicle"));
+        System.out.println(bundle.getString("enter_matricule"));
+        try {
+            String matricule = sc.next();
+            int vehiculeToFind = Parking.retirerVehicule(matricule);
+            if(vehiculeToFind < 0){
+                throw new VehiculeNotFoundException(bundle.getString("vehicle_not_found"));
+            }
+
+            System.out.println(bundle.getString("vehicle_removed"));
+            System.out.println(
+                    bundle.getString("free_places")
+                            .replace("{0}", Integer.toString(Parking.placesDisponibles()))
+            );
+        } catch (VehiculeNotFoundException e) {
+            System.out.println(e);
+        }
+    }
+
+    private static void displayParkingOption(){
+        System.out.println(bundle.getString("display_parking"));
+        try {
+            Parking.displayParking();
+        } catch (ParkingEmptyException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void displayTotalAmountOption(){
+        System.out.println(bundle.getString("display_amount"));
+        System.out.println(Parking.getTotalAmount());
     }
 }
